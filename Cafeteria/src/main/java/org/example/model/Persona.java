2f25f756ddfac5f09b21a071abdb3fd14051069a
Gabriel A. Orellana <gabi.ale.orellana@gmail.com>
2026-05-04 22:26:44 -0300

    Se generan metodos getters y setters para la clase Persona

--- Cafeteria/src/main/java/org/example/model/Persona.java
+++ Cafeteria/src/main/java/org/example/model/Persona.java
@@ -12,6 +12,32 @@ public abstract class Persona {
         this.mail = mail;
     }
 
+    //getter
+
+    public String getDni() {
+        return dni;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    //setter
 
 
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
 }
