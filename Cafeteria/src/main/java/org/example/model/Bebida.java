1e7d28544a535fa1c2c3ed4b954dada4188df2d6
Gabriel A. Orellana <gabi.ale.orellana@gmail.com>
2026-05-04 21:06:27 -0300

    Se crean las clases Bebida y Comida, que extienden de Producto.

---
+++ Cafeteria/src/main/java/org/example/model/Bebida.java
@@ -0,0 +1,33 @@
+package org.example.model;
+
+public class Bebida extends Producto{
+
+    private TamanioBebida tamanio;
+    private boolean esCaliente;
+
+    public Bebida(String nombre,TamanioBebida tamanio, double precio,boolean esCaliente, int stock){
+        super(nombre, stock, precio);
+        this.tamanio = tamanio;
+        this.esCaliente = esCaliente;
+    }
+
+    //getters
+
+    public TamanioBebida getTamanio() {
+        return tamanio;
+    }
+
+    public boolean isEsCaliente() {
+        return esCaliente;
+    }
+
+
+    //setters
+    public void setTamanio(TamanioBebida tamanio) {
+        this.tamanio = tamanio;
+    }
+
+    public void setEsCaliente(boolean esCaliente) {
+        this.esCaliente = esCaliente;
+    }
+}
